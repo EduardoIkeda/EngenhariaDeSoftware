02bbad5fae1083edf8fd28ee3f2faebc06baf9ee
Washington Júnior <wash.j5@gmail.com>
2024-09-20 18:31:45 -0300

    [UPDATE] Diagramas atualizados, projeto recriado para permitir sua publicação em site

---
+++ Publish/scripts/processElementData.js
@@ -0,0 +1,77 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_d_QuwXeCEe-lIIqoq4P27g,_nX41kXeDEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/Review_78D29C04_23f6ece9_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_d_QuwXeCEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/Sprint_4B5A778F_b9632591_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_d_QuwXeCEe-lIIqoq4P27g,_mjeWEXeDEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/Milestone_84AB1EB2_2fcf6f97_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_d_QuwXeCEe-lIIqoq4P27g,_knysgXeDEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/Planning_5CAAF81E_7cf4903_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_fvhygXeCEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/EndProject_3739A301_a5425103_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_d_QuwXeCEe-lIIqoq4P27g,_loRSEXeDEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/Execution_D575BF87_809a106c_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/ProjetoSus_A67AC9AE_a67ac9ae_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_Y4UDEXeCEe-lIIqoq4P27gActivity"]="./../../new_plug-in/deliveryprocesses/resources/Warmup_4256AF8C_b05f5d8e_Activity.jpeg"
+contentPage.processPage.imageFiles["_RbHRsXeCEe-lIIqoq4P27g,_Y4UDEXeCEe-lIIqoq4P27gActivityDetail"]="./../../new_plug-in/deliveryprocesses/resources/Warmup_4256AF8C_b05f5d8e_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_C1h2cXeEEe-lIIqoq4P27g"] = ["Revisar diagramas da uml","new_plug-in/tasks/revisar_diagramas_da_uml_C0918E38.html"];
+contentPage.processPage.elementUrls["_RbHRsXeCEe-lIIqoq4P27g"] = ["ProjetoSus","new_plug-in/deliveryprocesses/ProjetoSus_A67AC9AE.html"];
+contentPage.processPage.elementUrls["_72-lZHeCEe-lIIqoq4P27g"] = ["Plano de equipe","new_plug-in/workproducts/plano_de_equipe_19AF5642.html"];
+contentPage.processPage.elementUrls["_728JJHeCEe-lIIqoq4P27g"] = ["Definir banco de dados","new_plug-in/tasks/definir_banco_de_dados_5963DF89.html"];
+contentPage.processPage.elementUrls["_728JJ3eCEe-lIIqoq4P27g"] = ["Definir diagrama de componentes","new_plug-in/tasks/definir_diagrama_de_componentes_4799775E.html"];
+contentPage.processPage.elementUrls["_728JIXeCEe-lIIqoq4P27g"] = ["Criar historias e tarefas","new_plug-in/tasks/criar_historias_e_tarefas_FF97D60.html"];
+contentPage.processPage.elementUrls["_729-VHeCEe-lIIqoq4P27g"] = ["Documentos de requisitos","new_plug-in/workproducts/documentos_de_requisitos_8CB0C80A.html"];
+contentPage.processPage.elementUrls["_72-lYneCEe-lIIqoq4P27g"] = ["Diagrama de componentes","new_plug-in/workproducts/diagrama_de_componentes_EE065059.html"];
+contentPage.processPage.elementUrls["_728JJneCEe-lIIqoq4P27g"] = ["Definir diagrama de classes","new_plug-in/tasks/definir_diagrama_de_classes_89C239B9.html"];
+contentPage.processPage.elementUrls["_9RY4cXeDEe-lIIqoq4P27g"] = ["Teste do cliente","new_plug-in/tasks/teste_do_cliente_C17532F5.html"];
+contentPage.processPage.elementUrls["_XVeFcneEEe-lIIqoq4P27g"] = ["Documentos de requisitos","new_plug-in/workproducts/documentos_de_requisitos_8CB0C80A.html"];
+contentPage.processPage.elementUrls["_XVeFYneEEe-lIIqoq4P27g"] = ["Desenvolver visual do prototipo","new_plug-in/tasks/desenvolver_visual_do_prototipo_D869B7C3.html"];
+contentPage.processPage.elementUrls["_729-XHeCEe-lIIqoq4P27g"] = ["Diagrama de entidade relacionamento","new_plug-in/workproducts/diagrama_de_entidade_relacionamento_32DD951F.html"];
+contentPage.processPage.elementUrls["_mjeWEXeDEe-lIIqoq4P27g"] = ["Milestone","new_plug-in/deliveryprocesses/Milestone_84AB1EB2.html"];
+contentPage.processPage.elementUrls["_729-UneCEe-lIIqoq4P27g"] = ["Governo","new_plug-in/roles/governo_19150184.html"];
+contentPage.processPage.elementUrls["_728JI3eCEe-lIIqoq4P27g"] = ["Criar prototipo de baixa fidelidade","new_plug-in/tasks/criar_prototipo_de_baixa_fidelidade_ACDC7E7F.html"];
+contentPage.processPage.elementUrls["_Y4UDEXeCEe-lIIqoq4P27g"] = ["Warmup","new_plug-in/deliveryprocesses/Warmup_4256AF8C.html"];
+contentPage.processPage.elementUrls["_729-UHeCEe-lIIqoq4P27g"] = ["Analista de Requisitos","new_plug-in/roles/analista_de_requisitos_80CF4093.html"];
+contentPage.processPage.elementUrls["_728JK3eCEe-lIIqoq4P27g"] = ["Teste do cliente","new_plug-in/tasks/teste_do_cliente_C17532F5.html"];
+contentPage.processPage.elementUrls["_XVeFZ3eEEe-lIIqoq4P27g"] = ["Testar integracao de api","new_plug-in/tasks/testar_integracao_de_api_4E06EA93.html"];
+contentPage.processPage.elementUrls["_C1h2cHeEEe-lIIqoq4P27g"] = ["Realizar reuniao de review","new_plug-in/tasks/realizar_reuniao_de_review_D4500094.html"];
+contentPage.processPage.elementUrls["_XVeFaneEEe-lIIqoq4P27g"] = ["Quadro Kanban","new_plug-in/workproducts/quadro_kanban_86E688E9.html"];
+contentPage.processPage.elementUrls["_729-WneCEe-lIIqoq4P27g"] = ["Diagrama de classes","new_plug-in/workproducts/diagrama_de_classes_766DEE41.html"];
+contentPage.processPage.elementUrls["_729-VneCEe-lIIqoq4P27g"] = ["Prototipo de baixa fidelidade","new_plug-in/workproducts/prototipo_de_baixa_fidelidade_2F2DB150.html"];
+contentPage.processPage.elementUrls["_XVeFbHeEEe-lIIqoq4P27g"] = ["Diagrama de caso de uso","new_plug-in/workproducts/diagrama_de_caso_de_uso_DF83087D.html"];
+contentPage.processPage.elementUrls["_XVeFaXeEEe-lIIqoq4P27g"] = ["Testar usabilidade","new_plug-in/tasks/testar_usabilidade_13658480.html"];
+contentPage.processPage.elementUrls["_XVeFY3eEEe-lIIqoq4P27g"] = ["Implementar regras de negocio","new_plug-in/tasks/implementar_regras_de_negocio_38C35673.html"];
+contentPage.processPage.elementUrls["_XVeFdHeEEe-lIIqoq4P27g"] = ["Codigo fonte do backend","new_plug-in/workproducts/codigo_fonte_do_backend_1DC307C1.html"];
+contentPage.processPage.elementUrls["_XVeFZneEEe-lIIqoq4P27g"] = ["Realizar reuniao diaria","new_plug-in/tasks/realizar_reuniao_diaria_957AE83F.html"];
+contentPage.processPage.elementUrls["_XVdeUHeEEe-lIIqoq4P27g"] = ["Atualizar kanban","new_plug-in/tasks/atualizar_kanban_B8398593.html"];
+contentPage.processPage.elementUrls["_728JJXeCEe-lIIqoq4P27g"] = ["Definir diagrama de caso de uso","new_plug-in/tasks/definir_diagrama_de_caso_de_uso_B4C5C8E8.html"];
+contentPage.processPage.elementUrls["_fvhygXeCEe-lIIqoq4P27g"] = ["EndProject","new_plug-in/deliveryprocesses/EndProject_3739A301.html"];
+contentPage.processPage.elementUrls["_XVeFZHeEEe-lIIqoq4P27g"] = ["Integrar api","new_plug-in/tasks/integrar_api_130596A5.html"];
+contentPage.processPage.elementUrls["_4ah3MHeDEe-lIIqoq4P27g"] = ["Implantar sistema","new_plug-in/tasks/implantar_sistema_5A877ABE.html"];
+contentPage.processPage.elementUrls["_728JIneCEe-lIIqoq4P27g"] = ["Criar prototipo de alta fidelidade","new_plug-in/tasks/criar_prototipo_de_alta_fidelidade_579C2391.html"];
+contentPage.processPage.elementUrls["_729-WHeCEe-lIIqoq4P27g"] = ["Prototipo de alta fidelidade","new_plug-in/workproducts/prototipo_de_alta_fidelidade_5A4DF5D9.html"];
+contentPage.processPage.elementUrls["_XVeFaHeEEe-lIIqoq4P27g"] = ["Testar integracao front e back end","new_plug-in/tasks/testar_integracao_front_e_back_end_575A56A.html"];
+contentPage.processPage.elementUrls["_9RY4cHeDEe-lIIqoq4P27g"] = ["Entregar modulo parcial","new_plug-in/tasks/entregar_modulo_parcial_507E2D47.html"];
+contentPage.processPage.elementUrls["_XVdeUXeEEe-lIIqoq4P27g"] = ["Desenvolver backend","new_plug-in/tasks/desenvolver_backend_65DDBB83.html"];
+contentPage.processPage.elementUrls["_XVeFYHeEEe-lIIqoq4P27g"] = ["Desenvolver banco de dados","new_plug-in/tasks/desenvolver_banco_de_dados_3DB2FE3B.html"];
+contentPage.processPage.elementUrls["_nX41kXeDEe-lIIqoq4P27g"] = ["Review","new_plug-in/deliveryprocesses/Review_78D29C04.html"];
+contentPage.processPage.elementUrls["_728JIHeCEe-lIIqoq4P27g"] = ["Coletar requisitos","new_plug-in/tasks/coletar_requisitos_D7E45E49.html"];
+contentPage.processPage.elementUrls["_728JKXeCEe-lIIqoq4P27g"] = ["Elaborar cronograma geral","new_plug-in/tasks/elaborar_cronograma_geral_1C75B92.html"];
+contentPage.processPage.elementUrls["_H5uwIXeEEe-lIIqoq4P27g"] = ["Elaborar cronograma da sprint","new_plug-in/tasks/elaborar_cronograma_da_sprint_C79EAF46.html"];
+contentPage.processPage.elementUrls["_72-lYHeCEe-lIIqoq4P27g"] = ["Diagrama de caso de uso","new_plug-in/workproducts/diagrama_de_caso_de_uso_DF83087D.html"];
+contentPage.processPage.elementUrls["_H5uwIHeEEe-lIIqoq4P27g"] = ["Detalhar requisitos","new_plug-in/tasks/detalhar_requisitos_4345C731.html"];
+contentPage.processPage.elementUrls["_XVeFZXeEEe-lIIqoq4P27g"] = ["Integrar front end e back end","new_plug-in/tasks/integrar_front_end_e_back_end_EFC6215A.html"];
+contentPage.processPage.elementUrls["_728JKneCEe-lIIqoq4P27g"] = ["Testar usabilidade","new_plug-in/tasks/testar_usabilidade_13658480.html"];
+contentPage.processPage.elementUrls["_XVeFcHeEEe-lIIqoq4P27g"] = ["Diagrama de entidade relacionamento","new_plug-in/workproducts/diagrama_de_entidade_relacionamento_32DD951F.html"];
+contentPage.processPage.elementUrls["_XVeFYXeEEe-lIIqoq4P27g"] = ["Desenvolver front end web","new_plug-in/tasks/desenvolver_front_end_web_9BF4A733.html"];
+contentPage.processPage.elementUrls["_728JKHeCEe-lIIqoq4P27g"] = ["Definir equipe","new_plug-in/tasks/definir_equipe_BF5AE1BF.html"];
+contentPage.processPage.elementUrls["_knysgXeDEe-lIIqoq4P27g"] = ["Planning","new_plug-in/deliveryprocesses/Planning_5CAAF81E.html"];
+contentPage.processPage.elementUrls["_XVeFbneEEe-lIIqoq4P27g"] = ["Diagrama de classes","new_plug-in/workproducts/diagrama_de_classes_766DEE41.html"];
+contentPage.processPage.elementUrls["_loRSEXeDEe-lIIqoq4P27g"] = ["Execution","new_plug-in/deliveryprocesses/Execution_D575BF87.html"];
+contentPage.processPage.elementUrls["_d_QuwXeCEe-lIIqoq4P27g"] = ["Sprint","new_plug-in/deliveryprocesses/Sprint_4B5A778F.html"];
